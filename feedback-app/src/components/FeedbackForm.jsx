f49f911f97dbd997a6f85e396045e9698ab579c3
Eroll Uka <erollfaceit@gmail.com>
2022-03-17 22:53:59 +0100

    Validation & Card Animation

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,48 +1,65 @@
-import {useState} from 'react'
-import RatingSelect from './RatingSelect'
-import Card from './shared/Card'
-import Button from './shared/Button'
+import { useState } from "react"
+import Card from "./shared/Card"
+import Button from "./shared/Button"
+import RatingSelect from "./RatingSelect"
 
-function FeedbackForm() {
-const [text , setText] = useState('')
-const [rating , setRating] = useState()
-const [btnDisabled , setBtnDisabled] = useState(true)
-const [message , setMessage] = useState('')
+function FeedbackForm({handleAdd}) {
+
+    const [text, setText] = useState('')
+    const [rating , setRating] = useState()
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
 
-const handleTextChange = (e) => {
-  if(text === ''){
-    setBtnDisabled(true)
-    setMessage(null)
-  }else if(text !== '' && text.trim().length <=10){
-    setMessage('Text must be atleast 10 characters')
-    setBtnDisabled(true)
-  }else {
-    setMessage(null)
-    setBtnDisabled(false)
-  }
-    setText(e.target.value)
-}
+    const handleTextChange = (e) => {
+        if(text === ""){
+            setBtnDisabled(true)
+            setMessage(null)
+        }
+        else if(text!=="" && text.trim().length <= 10){
+            setMessage("Text must be at least 10 characters")
+            setBtnDisabled(true)
+        }
+        else{
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
+
+        setText(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10){
+            const newFeedback = {
+                 text,
+                 rating
+            }
+            handleAdd(newFeedback)
+
+            setText('')
+        }
+    }
 
   return (
-    <Card>        
-        <form>
+    <Card>
+        <form onSubmit={handleSubmit}>
             <h2>How would you rate your service with us?</h2>
-        <RatingSelect select={(rating) => setRating(rating)}/>
-        <div className = 'input-group'>
-        <input 
-        onChange={handleTextChange} type ='text' 
-        placeholder= 'Write a review'
-        value={text} />
-        <Button type='submit' version='secondary' isDisabled={btnDisabled}>Send</Button>
-        </div>
-
-      {message && <div className='message'>{message}</div>}
-
+            <RatingSelect select={(rating) => setRating(rating)}/>
+            <div className="input-group">
+                <input 
+                onChange={handleTextChange}
+                type='text'
+                placeholder="Write us something!"
+                value={text}
+                />
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+            </div>
+            {message && <div className='message'>{message}</div>}
         </form>
-        
     </Card>
   )
 }
 
-export default FeedbackForm
+export default FeedbackForm 
