910afc784a99e25e337b8bd4c7126858569b0282
Eroll Uka <erollfaceit@gmail.com>
2022-03-17 17:41:38 +0100

    Added RatingSelect
    Added a component to choose a numeric rating from 1-10 when submitting feedback.

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,10 +1,26 @@
 import {useState} from 'react'
-import Card
- from "./shared/Card"
+import RatingSelect from './RatingSelect'
+import Card from './shared/Card'
+import Button from './shared/Button'
+
 function FeedbackForm() {
 const [text , setText] = useState('')
+const [rating , setRating] = useState()
+const [btnDisabled , setBtnDisabled] = useState(true)
+const [message , setMessage] = useState('')
+
 
 const handleTextChange = (e) => {
+  if(text === ''){
+    setBtnDisabled(true)
+    setMessage(null)
+  }else if(text !== '' && text.trim().length <=10){
+    setMessage('Text must be atleast 10 characters')
+    setBtnDisabled(true)
+  }else {
+    setMessage(null)
+    setBtnDisabled(false)
+  }
     setText(e.target.value)
 }
 
@@ -12,14 +28,17 @@ const handleTextChange = (e) => {
     <Card>        
         <form>
             <h2>How would you rate your service with us?</h2>
-        {/* @to do rating component */}
+        <RatingSelect select={(rating) => setRating(rating)}/>
         <div className = 'input-group'>
         <input 
         onChange={handleTextChange} type ='text' 
         placeholder= 'Write a review'
         value={text} />
-        <button type="submit">Send</button>
+        <Button type='submit' version='secondary' isDisabled={btnDisabled}>Send</Button>
         </div>
+
+      {message && <div className='message'>{message}</div>}
+
         </form>
         
     </Card>
